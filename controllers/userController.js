f700af09f32c5c840c9f21995c9e3bc7702e47bc
Carson DiPangrazio <carson.dipangrazio@gmail.com>
2023-02-09 21:33:14 -0500

    bleh

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,5 +2,13 @@ const { ObjectId } = require('mongoose').Types;
 const { User, Thought } = require('../models')
 
 module.exports = {
-    
+    getUsers (req, res) {
+        User.find()
+            .then(async (users) =>{
+                const userObj = {
+                    users,
+                }
+            })
+            .catch((err) =>)
+    }
 }
