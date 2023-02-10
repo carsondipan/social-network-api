262c44963a282a3878db650f067fcb38d67f162b
Carson DiPangrazio <carson.dipangrazio@gmail.com>
2023-02-09 20:57:17 -0500

    fixed thoughts.js

--- models/thought.js
+++ models/thought.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const reactionSchema = new mongoose.Schema(
+const thoughtSchema = new mongoose.Schema(
     {
         thoughtText: {
             type: String,
@@ -14,8 +14,67 @@ const reactionSchema = new mongoose.Schema(
             type: String,
             required: true,
         },
-        reactions: {
-        }
+        reactions: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Reaction',
+            },
+        ],
 
+    },
+);
+
+const reactionSchema = new mongoose.Schema(
+    {
+        reactionId: {
+            type: mongoose.Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String, 
+            required: true
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            type: Date, default: Date.now
+        },
     }
 );
+
+thoughtSchema
+    .virtual('reactionCount')
+    .get(function () {
+        return Object.keys(this.reactions).length;
+    }
+);
+
+const Thought = mongoose.model('Thought', thoughtSchema);
+const Reaction = mongoose.model('Reaction', reactionSchema);
+
+const errHandler = (err) => console.error(err);
+
+const init = async () => {
+    await Reaction.deleteMany({});
+    await Reaction.create(
+        {
+            reactionBody: 'beans haha',
+            username: 'jawncena',
+        },
+        (err) => (err ? errHandler(err) : console.log('Created new reaction'))
+    );
+    await Thought.deleteMany({});
+    await Thought.create(
+        {
+            thoughtText: 'dude what if we cooked it with gas',
+            username: 'jawncena'
+        },
+        (err) => (err ? errHandler(err) : console.log('Created new thought'))
+    );
+};
+
+init();
+
+module.exports = Thought, Reaction;
